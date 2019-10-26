db786e639b9d02d64e7e3189b5e7ff936722950f
Jonathan Alexander Gibson <agibson@gibsonusa.net>
2019-10-26 17:20:58 -0500

    Java work, MD logo, GUI Example

--- Java/Java Coding Reference.java
+++ Java/Java Coding Reference.java
@@ -12,51 +12,6 @@ Java is very similar to C and C++ syntactically.
 
 // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-public class Printing
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World!");
-    call_func();
-  }
-
-  public static void call_func()
-	{
-		System.out.println("Printing another line.");
-	}
-}
-
-// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
-public class Store
-{
-  // instance fields
-  String productType;
-  int inventoryCount;
-  double inventoryPrice;
-  
-  /* Constructor method */
-  public Store(String product, int count, double price)
-  {
-    productType = product;
-    inventoryCount = count;
-    inventoryPrice = price;
-  }
-  
-  /* Main method */
-  public static void main(String[] args)
-  {
-    Store lemonadeStand = new Store("lemonade", 42, .99);
-    Store cookieShop = new Store("cookies", 12, 3.75);
-    
-    System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
-    
-    System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
-  }
-}
-
-// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
 String
   // the '+' operator works for variable types that are not explicit strings
 	.equals(<string_name>) // returns a boolean that tells whether the two strings are equal or not
@@ -73,6 +28,13 @@ long
 float
 double
 
+void // there is no specific output from a method with this datatype
+
+// Access and Reference Method Modifiers
+public // other classes can access method with this modifier
+private // other classes can not access method with this modifier ; only methods from within the same class
+static // allows a method to be called without explicitly referencing a class instance
+
 // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 true
@@ -86,6 +48,13 @@ null
 *
 /
 %
+
++=
+-=
+*=
+/=
+%=
+
 =
 
 // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -100,8 +69,140 @@ null
 // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 &&
-||
+||;
 
 // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
+public class Printing
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+    call_func();
+  }
+
+  public static void call_func()
+  {
+    System.out.println("Printing another line.");
+  }
+}
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+public class Store
+{
+  // instance fields are usable by all methods in a particular class
+  String productType;
+  int inventoryCount;
+  double inventoryPrice;
+  
+  /* Constructor method */
+  public Store(String product, int count, double price)
+  {
+    productType = product;
+    inventoryCount = count;
+    inventoryPrice = price;
+  }
+
+  // advertise method
+  public void advertise()
+  {
+    String message = "Selling " + productType + "!";
+    System.out.println(message);
+  }
+  
+  public void greetCustomer(String customer)
+  {
+    System.out.println("Welcome to the store, " + customer + "!");
+  }
+
+  public String info()
+  {
+    return "This store sells " + productType + " at a price of " + price + ". There are " + inventoryCount + " items left.";
+  }
+  
+  /* Main method */
+  public static void main(String[] args)
+  {
+    Store lemonadeStand = new Store("lemonade", 42, .99);
+    Store cookieShop = new Store("cookies", 12, 3.75);
+    
+    System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
+    System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
+    System.out.println(lemonadeStand); // will print "Store@<instance_memory_addr>"
+    System.out.println(cookieShop); // will print "Store@<instance_memory_addr>"
+  }
+} // Store class
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+public class SavingsAccount
+{
+  int balance;
+  
+  public SavingsAccount(int initialBalance)
+  {
+    balance = initialBalance;
+  }
+  
+  public void checkBalance()
+  {
+    System.out.println("Hello!");
+    System.out.println("Your balance is " + balance);
+  }
+  
+  public void deposit(int amountToDeposit)
+  {
+    balance = balance + amountToDeposit;
+    System.out.println("You just deposited " + amountToDeposit);
+  }
+  
+  public int withdraw(int amountToWithdraw)
+  {
+    balance -= amountToWithdraw;
+    System.out.println("You just withdrew " + amountToWithdraw);
+    return amountToWithdraw;
+  }
+  
+  public static void main(String[] args)
+  {
+    SavingsAccount savings = new SavingsAccount(2000);
+    savings.checkBalance(); //Check balance:
+    int less = savings.withdraw(300); //Withdrawing:
+    savings.checkBalance(); //Check balance:
+    savings.deposit(600); //Deposit:
+    savings.checkBalance(); //Check balance:
+    savings.deposit(600); //Deposit:
+    savings.checkBalance(); //Check balance:
+  }       
+} // Savings class
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+public class Dog {
+  String breed;
+  boolean hasOwner;
+  int age;
+  
+  public Dog(String dogBreed, boolean dogOwned, int dogYears) {
+    System.out.println("Constructor invoked!");
+    breed = dogBreed;
+    hasOwner = dogOwned;
+    age = dogYears;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Main method started");
+    Dog fido = new Dog("poodle", false, 4);
+    Dog nunzio = new Dog("shiba inu", true, 12);
+    boolean isFidoOlder = fido.age > nunzio.age;
+    int totalDogYears = nunzio.age + fido.age;
+    System.out.println("Two dogs created: a " + fido.breed + " and a " + nunzio.breed);
+    System.out.println("The statement that fido is an older dog is: " + isFidoOlder);
+    System.out.println("The total age of the dogs is: " + totalDogYears);
+    System.out.println("Main method finished");
+  }
+} // Dog class
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
--- Java/Java Management.md
+++ Java/Java Management.md
@@ -1,5 +1,8 @@
 <!-- Java Management.md -->
 
+# <img src="../.pics/Lexxeous/lexx_headshot_clear.png" width="100px"/> Lexxeous's Java Management: <img src="../.pics/Java/java_logo.png" width="100"/>
+### Manage Java Projects.
+
 A Java program is essentially a collection of 1 or more Java files that have classes, `main` functions, and supplementary functions.
 Upon executing a particular file, the `main` function will be called. The functionality depends on the other functions that are called within the `main` and the other files that are included for the primary class to reference.
 
@@ -7,3 +10,25 @@ Upon executing a particular file, the `main` function will be called. The functi
 javac <filename>.java // creates a <filename>.class file to be executed
 java <filename> // executes the <filename>.class "main" function
 ```
+
+### Unchecked or Unsafe Operations
+
+The following warning is displayed when you execute code which the Java compiler considers to be lacking in error-checking, or is potentially unsafe in some way.
+
+```bash
+Note: <filename>.java uses unchecked or unsafe operations.
+Note: Recompile with -Xlint:unchecked for details.
+```
+
+This warning can also come up in Java 5 and later if you're using collections without type specifiers (e.g., `Arraylist()` instead of `ArrayList<String>()`). It means that the compiler can't check that you're using the collection in a type-safe way, using generics. <br>
+
+This warning can be suppressed or fixed by using the `-Xlint` flag.
+
+```java
+// You can use the flag in this format to check the lines that Java beleives to be unsafe, and fix them.
+javac -Xlint:unchecked <filename>.java
+
+// You can use the flag with this format to suppress the warning and compile the Java code, despite the warning.
+javac <filename>.java -Xlint
+```
+
