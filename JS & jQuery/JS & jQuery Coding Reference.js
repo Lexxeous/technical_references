a944d79ba40a11e02d7809c9c1550bf71916e938
Jonathan Alexander Gibson <agibson@gibsonusa.net>
2020-01-28 15:33:04 -0600

    Started JS Promises and Anaconda references

--- JS & jQuery/JS & jQuery Coding Reference.js
+++ JS & jQuery/JS & jQuery Coding Reference.js
@@ -933,6 +933,95 @@ console.log(nurseOlynyk.certifications); // ['Trauma', 'Pediatrics', 'Genetics']
 // PROMISES
 
 
+/*
+In web development, asynchronous programming is notorious for being a challenging topic.
+
+An asynchronous operation is one that allows the computer to “move on” to other tasks while waiting for the asynchronous operation to complete.
+Asynchronous programming means that time-consuming operations don’t have to bring everything else in our programs to a halt.
+
+There are countless examples of asynchronicity in our everyday lives.
+Cleaning our house, for example, involves asynchronous operations such as a dishwasher washing our dishes or a washing machine washing our clothes.
+While we wait on the completion of those operations, we’re free to do other chores.
+
+Similarly, web development makes use of asynchronous operations.
+Operations like making a network request or querying a database can be time-consuming,
+but JavaScript allows us to execute other tasks while awaiting their completion.
+
+Modern JavaScript handles asynchronicity using the Promise object, introduced with ES6.
+
+Promises are objects that represent the eventual outcome of an asynchronous operation. A Promise object can be in one of three states:
+
+1. Pending:
+		The initial state; the operation has not completed yet.
+2. Fulfilled:
+		The operation has completed successfully and the promise now has a resolved value.
+		For example, a request’s promise might resolve with a JSON object as its value.
+3. Rejected:
+		The operation has failed and the promise has a reason for the failure.
+		This reason is usually an Error of some kind.
+
+We refer to a promise as settled if it is no longer pending; it is either fulfilled or rejected.
+All promises eventually settle, enabling us to write logic for what to do if the promise fulfills or if it rejects.
+
+The Promise constructor method takes a function parameter called the executor function which runs automatically when the constructor is called.
+The executor function generally starts an asynchronous operation and dictates how the promise should be settled.
+
+The executor function has two function parameters, usually referred to as the resolve() and reject() functions.
+The resolve() and reject() functions aren’t defined by the programmer.
+When the Promise constructor runs, JavaScript will pass its own resolve() and reject() functions into the executor function.
+
+• The resolve parameter is a function with one argument.
+	Under the hood, if invoked, resolve() will change the promise’s status from pending to fulfilled,
+	and the promise’s resolved value will be set to the argument passed into resolve().
+• The reject parameter is a function that takes a reason or error as an argument.
+	Under the hood, if invoked, reject() will change the promise’s status from pending to rejected,
+	and the promise’s rejection reason will be set to the argument passed into reject().
+*/
+
+
+const inventory = {
+  sunglasses: 0,
+  pants: 1088,
+  bags: 1344
+};
+
+const myExecutor = (resolve, reject) => {
+  if(inventory.sunglasses > 0) {
+    resolve("Sunglasses order processed.");
+  }
+  else {
+    reject("That item is sold out.");
+  }
+};
+
+function orderSunglasses() {
+  return new Promise(myExecutor);
+}
+
+orderPromise = orderSunglasses();
+console.log(orderPromise);
+
+/* 
+	Function setTimeout() is a Node API (a comparable API is provided by web browsers) uses callback functions to schedule tasks to be performed after a delay.
+	It has two parameters: a callback function and a delay in milliseconds.
+
+	If we invoke setTimeout() with the callback function func_name() and 2000. In at least two seconds func_name() will be invoked.
+	But why is it “at least” two seconds and not exactly two seconds?
+
+	This delay is performed asynchronously; the rest of our program won’t stop executing during the delay.
+	Asynchronous JavaScript uses something called the event-loop. After two seconds, func_name() is added to a line of code waiting to be run.
+	Before it can run, any synchronous code from the program will run.
+	Next, any code in front of it in the line will run. This means it might be more than two seconds before func_name() is actually executed.
+*/
+
+
+const returnPromiseFunction = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(( ) => {resolve('I resolved!')}, 1000);
+  });
+};
+
+const prom = returnPromiseFunction();
 
 
 /*--------------------------------------------------------------------------------------------------------------*/
---
+++ Python/Conda V_ENV Reference.md
@@ -0,0 +1,62 @@
+# <img src="../.pics/Lexxeous/lexx_headshot_clear.png" width="90"/> Lexxeous's Conda V_ENV Management <img src="../.pics/Python/conda_logo.png" width="90"/>
+
+### Create/Delete Environment(s)
+
+Create `Anaconda` virtual environment (with `pip` backend) to install `requirements.txt` packages:
+
+```python
+conda create -n <virtual_env_name> python=<X.Y.Z> pip # create V_ENV with pip backend
+conda install pip # set pip as package manager for <virtual_env_name>
+pip install -r requirements.txt # install all specified packages within "requirements.txt" for <virtual_env_name>
+```
+
+Create `Anaconda` virtual environment (with `anaconda` backend) with all global packages:
+
+```python
+conda create -n <virtual_env_name> python=<X.Y.Z> anaconda
+```
+
+Create `Anaconda` virtual environment (with `anaconda` backend) to install minimal packages:
+
+```python
+conda create -n <virtual_env_name> python=<X.Y.Z>
+conda install --yes --file requirements.txt
+```
+
+Delete an `Anaconda` virtual environment:
+```python
+conda remove -n <virtual_env_name> --all
+```
+
+### Activate/Deactivate Environment(s)
+
+Activate `Anaconda` virtual environment:
+```python
+conda activate <virtual_env_name>
+```
+
+Deactivate the `Anaconda` virtual environment:
+```python
+conda deactivate
+```
+
+### Install/Uninstall Package(s)
+
+Install/Uninstall individual package to/from an `Anaconda` virtual environment:
+```python
+conda install -n <virtual_env_name> <pkg_name>
+conda uninstall -n <virtual_env_name> <pkg_name>
+```
+
+### Misc
+
+View `Anaconda` virtual environment information:
+```python
+conda info # returns information about your Anaconda environment
+conda info -e # view all available `Anaconda` virtual environments
+```
+
+View list of packages installed for an individual `Anaconda` virtual environment:
+```python
+conda list -n <virtual_env_name>
+```
