f06935a5b6e1e4fd1cb4c7b73c1e445de754aaf6
Jonathan Alexander Gibson <agibson@gibsonusa.net>
2020-02-19 18:15:55 -0600

    Assembly (ELF) related Python script

--- Python/Python Coding Reference.py
+++ Python/Python Coding Reference.py
@@ -1,4 +1,19 @@
-# Python Coding Reference
+# Python Coding Reference.py
 
-python <file_name.py> install
-python3 <file_name.py> install
+# Indentation in Python is paramount, there are no curly brackets to distinguish blocks of code like in C.
+# You also cant mix tabs and spaces for indentation in a single file, you must choose one and be consistent.
+
+/ # floating point division
+// #integer division
+
+def func_name(param1, param2):
+	''' If you place on of these on the first line of a function definition, it will come up for the help documentation for the file '''
+	return var
+
+
+# While in a python environment, you can import modules and display its documentation with:
+>>> import <module_name> as <MN>
+>>> dir(<MN>) # documentation on module targets
+>>> <MN>.__doc__ # general module documentation
+>>> <MN>.<func_name>.__doc__ # documentation on a single function from the module
+>>> help(<MN>) # displays full general documentation for module
